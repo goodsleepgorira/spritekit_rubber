13d81998f96b8fbdf7be827e727153127b64dfa3
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-08-31 00:44:39 -0400

    Test

--- TestMoveAction/TestScene.swift
+++ TestMoveAction/TestScene.swift
@@ -6,14 +6,23 @@ import Foundation
 import SpriteKit
 
 class TestScene:SKScene {
+
     var birdBrown:SKSpriteNode!
+    var birdRed:SKSpriteNode!
 
     //現在シーン設定時の呼び出しメソッド
     override func didMoveToView(view: SKView) {
         
         //SKSファイルに配置した鳥ノードを取得する。
         birdBrown = self.childNodeWithName("bird_brown") as? SKSpriteNode
+        birdRed = self.childNodeWithName("bird_red") as? SKSpriteNode
         
+        //シーンを画面サイズに合わせる。
+        self.scaleMode = .AspectFit
+        
+        //画面端に物理ボディを設定する。
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+
     }
     
     
@@ -22,13 +31,12 @@ class TestScene:SKScene {
         
         //タッチした座標のノードを取得する。
         let location = touches.first!.locationInNode(self)
-        let node = nodeAtPoint(location) as! SKSpriteNode
-        
-        //鳥ノードの場合、物理ボディを設定する。
-        if(node == birdBrown){
-            node.physicsBody?.affectedByGravity = true
+
+        if let node = nodeAtPoint(location) as? SKSpriteNode {
+            //鳥ノードの場合、物理ボディを設定する。
+            if(node == birdBrown || node == birdRed){
+                node.physicsBody?.affectedByGravity = true
+            }
         }
-        
-    }
-    
+    }    
 }
