34b20529edef840255814c1a39d18c356e3d73ae
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-09-03 23:46:01 +1200

    Test

--- TestMoveAction/TestScene.swift
+++ TestMoveAction/TestScene.swift
@@ -7,30 +7,29 @@ import SpriteKit
 
 class TestScene:SKScene {
 
-    var birdBrown:SKSpriteNode!
-    var birdRed:SKSpriteNode!
-    var birdBlue:SKSpriteNode!
+    var rocketBrown:SKSpriteNode!
+    var rocketRed:SKSpriteNode!
+    var rocketBlue:SKSpriteNode!
 
 
     //現在シーン設定時の呼び出しメソッド
     override func didMoveToView(view: SKView) {
         
-        //SKSファイルに配置した鳥ノードを取得する。
-        birdBrown = self.childNodeWithName("bird_brown") as? SKSpriteNode
-        birdRed = self.childNodeWithName("bird_red") as? SKSpriteNode
-        birdBlue = self.childNodeWithName("bird_blue") as? SKSpriteNode
+        //SKSファイルに配置した宇宙船ノードを取得する。
+        rocketBrown = self.childNodeWithName("rocket_brown") as? SKSpriteNode
+        rocketRed = self.childNodeWithName("rocket_red") as? SKSpriteNode
+        rocketBlue = self.childNodeWithName("rocket_blue") as? SKSpriteNode
         
         //シーンを画面サイズに合わせる。
         self.scaleMode = .AspectFit
         
         //画面端に物理ボディを設定する。
         self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
-
     }
     
     
     //画面タッチ時の呼び出しメソッド
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        print("画面をタップしました")
-    }    
+        print("画面をタップしました。")
+    }
 }
