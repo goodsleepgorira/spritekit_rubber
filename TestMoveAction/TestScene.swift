d347a0700b33d35cb9c5c6cc000bd7b7a534ef85
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-09-05 12:08:46 +1200

    Test

--- TestMoveAction/TestScene.swift
+++ TestMoveAction/TestScene.swift
@@ -13,8 +13,6 @@ class TestScene:SKScene {
         //シーンを画面サイズに合わせる。
         self.scaleMode = .AspectFit
         
-        //画面端に物理ボディを設定する。
-        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
     }
     
     
